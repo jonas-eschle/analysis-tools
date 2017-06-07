f2195380f4c5c5bf27effdc00f51870a71ddb9cf
Albert Puig <albert.puig@cern.ch>
2017-06-07 15:21:07 +0200

    Add the possibility to choose batch system.

--- analysis/batch/__init__.py
+++ analysis/batch/__init__.py
@@ -8,11 +8,15 @@
 """Batch managing utils."""
 
 
-def get_batch_system():
-    """Detect and get the batch system.
+def get_batch_system(name=None):
+    """Get the batch system, optionally detecting it from the system.
 
     If two batch systems are present, the priority is defined by the order of
     the `analysis.batch.batch_system.BATCH_SYSTEMS` OrderedDict.
+
+    Arguments:
+        name (str, optional): Name of the batch system to get. Defaults to
+            None, in which case the batch system detection is activated.
 
     Returns:
         str: Detected batch system.
@@ -22,9 +26,14 @@ def get_batch_system():
 
     """
     from analysis.batch.batch_system import BATCH_SYSTEMS
-    for batch_system in BATCH_SYSTEMS.values():
-        if batch_system.is_available:
+    if name:
+        batch_system = BATCH_SYSTEMS.get(name, None)
+        if batch_system:
             return batch_system
+    else:
+        for batch_system in BATCH_SYSTEMS.values():
+            if batch_system.is_available:
+                return batch_system
     raise ValueError()
 
 
