3986b7cb3f7ef812de632ef31846af352a538ba4
Jonas 'Mayou36' Eschle <mayou36@jonas.eschle.com>
2017-10-05 15:35:40 +0200

    no longer popping 'yield'

--- analysis/physics/__init__.py
+++ analysis/physics/__init__.py
@@ -120,7 +120,7 @@ def configure_model(config, shared_vars=None, external_vars=None):
     def configure_factory(observable, config, shared_vars):
         logger.debug("Configuring factory -> %s", config)
         if 'yield' in config:
-            yield_ = config.pop('yield')
+            yield_ = config['yield']
             if 'yield' not in shared_vars:
                 shared_vars['yield'] = sanitize_parameter(yield_, 'Yield', 'Yield')
         # if 'yield' in shared_vars:
@@ -158,7 +158,7 @@ def configure_model(config, shared_vars=None, external_vars=None):
                                                                                 factory_config,
                                                                                 shared_vars['pdf'][observable]))
                                                              for observable, factory_config
-                                                             in config.pop('pdf').items()),
+                                                             in config.get('pdf').items()),
                                                  parameters=config)
                                                  # parameters={param_name: (param_val, None)
                                                  #             for param_name, param_val in params.items()})
@@ -175,9 +175,9 @@ def configure_model(config, shared_vars=None, external_vars=None):
             #                                  for param_name, param_val
             #                                  in config.get('parameters', {}).items()})
             if 'yield' in shared_vars[pdf_name]:
-                yields[pdf_name] = shared_vars[pdf_name].pop('yield')
+                yields[pdf_name] = shared_vars[pdf_name]['yield']
             if 'yield' in pdf_config:
-                yield_ = pdf_config.pop('yield')
+                yield_ = pdf_config['yield']
                 if pdf_name not in yields:
                     yields[pdf_name] = sanitize_parameter(yield_, 'Yield', 'Yield')
                     # yields[pdf_name][0].setStringAttribute('shared', 'true')
@@ -200,9 +200,9 @@ def configure_model(config, shared_vars=None, external_vars=None):
                 if yields.keys()[-1] == factories.keys()[-1]:  # The last one should not have a yield!
                     raise ConfigError("Wrong order in yield/factory specification")
                 if 'yield' in config:
-                    yield_ = config.pop('yield')
+                    yield_ = config['yield']
                     if 'yield' not in shared_vars:
-                        parameters['yield'] = sanitize_parameter(config.pop('yield'), 'Yield', 'Yield')
+                        parameters['yield'] = sanitize_parameter(yield_, 'Yield', 'Yield')
                 # if 'yield' in parameters:
                 #     parameters['yield'][0].setStringAttribute('shared', 'true')
             return factory.SumPhysicsFactory(factories, yields, parameters)
