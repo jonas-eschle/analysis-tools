c1aa33ffebfbd50a6b20ed69b84d16a55a4d2c89
Albert Puig <albert.puig@cern.ch>
2017-11-23 10:39:36 +0100

    Bugfix in loaders.

--- analysis/utils/root.py
+++ analysis/utils/root.py
@@ -52,7 +52,7 @@ def load_library(name, lib_dirs=None, debug=False, force=False, use_mathmore=Fal
         if name == file_name:
             break
     if not os.path.exists(name):
-        raise OSError("Cannot locate library")
+        raise OSError("Cannot locate library -> {}".format(name))
     options = 'k'
     if force:
         options += "f"
