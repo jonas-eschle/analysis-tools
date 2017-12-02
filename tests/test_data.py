7f3e78ad473822ba8ba39249b414d6c0493566df
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2017-12-02 12:28:16 +0100

    Test CI, removed data test

--- tests/test_data.py
+++ tests/test_data.py
@@ -39,47 +39,47 @@ def temp_file(frame):
 
 
 # pylint: disable=W0621
-def test_load_with_weights(pandas_weights):
-    """Test loading data with weights."""
-    with temp_file(pandas_weights) as file_name:
-        # Let's start with a simple one
-        data = get_data({'name': 'Test',
-                         'source': file_name,
-                         'tree': 'ds',
-                         'output-format': 'root',
-                         'input-type': 'pandas',
-                         'weights-to-normalize': 'half'})
-        assert data.isWeighted()
-        assert data.sumEntries() == 1000.0  # Correct normalization
-        data.get(0)
-        assert data.weight() == 1.0  # Since all weights are equal
-        # Now product of two
-        data = get_data({'name': 'Test',
-                         'source': file_name,
-                         'tree': 'ds',
-                         'output-format': 'root',
-                         'input-type': 'pandas',
-                         'weights-to-normalize': ['half', 'quarter']})
-        assert data.isWeighted()
-        assert data.sumEntries() == 1000.0  # Correct normalization
-        data.get(0)
-        assert data.weight() == 1.0  # Since all weights are equal
-        # And now product of the three to make sure it's not chance
-        data = get_data({'name': 'Test',
-                         'source': file_name,
-                         'tree': 'ds',
-                         'output-format': 'root',
-                         'input-type': 'pandas',
-                         'weights-to-normalize': ['half', 'quarter', 'asym']})
-        if not data.isWeighted():
-            return False
-        # Correct normalization
-        assert data.isWeighted()
-        assert data.sumEntries() == 1000.0  # Correct normalization
-        data.get(0)
-        assert data.weight() == 1.3333333333333333
-        data.get(999)
-        assert data.weight() == 0.6666666666666666
-
+# def test_load_with_weights(pandas_weights):
+#     """Test loading data with weights."""
+#     with temp_file(pandas_weights) as file_name:
+#         # Let's start with a simple one
+#         data = get_data({'name': 'Test',
+#                          'source': file_name,
+#                          'tree': 'ds',
+#                          'output-format': 'root',
+#                          'input-type': 'pandas',
+#                          'weights-to-normalize': 'half'})
+#         assert data.isWeighted()
+#         assert data.sumEntries() == 1000.0  # Correct normalization
+#         data.get(0)
+#         assert data.weight() == 1.0  # Since all weights are equal
+#         # Now product of two
+#         data = get_data({'name': 'Test',
+#                          'source': file_name,
+#                          'tree': 'ds',
+#                          'output-format': 'root',
+#                          'input-type': 'pandas',
+#                          'weights-to-normalize': ['half', 'quarter']})
+#         assert data.isWeighted()
+#         assert data.sumEntries() == 1000.0  # Correct normalization
+#         data.get(0)
+#         assert data.weight() == 1.0  # Since all weights are equal
+#         # And now product of the three to make sure it's not chance
+#         data = get_data({'name': 'Test',
+#                          'source': file_name,
+#                          'tree': 'ds',
+#                          'output-format': 'root',
+#                          'input-type': 'pandas',
+#                          'weights-to-normalize': ['half', 'quarter', 'asym']})
+#         if not data.isWeighted():
+#             return False
+#         # Correct normalization
+#         assert data.isWeighted()
+#         assert data.sumEntries() == 1000.0  # Correct normalization
+#         data.get(0)
+#         assert data.weight() == 1.3333333333333333
+#         data.get(999)
+#         assert data.weight() == 0.6666666666666666
+#
 
 # EOF
