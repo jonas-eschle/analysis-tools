61b434e1ca0d574b19e330be7e01ce2287bc1ca2
Jonas Eschle 'Mayou36 <jonas.eschle@cern.ch>
2018-02-12 12:29:14 +0100

    Update setup.py values

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(os.path.join(here, 'README.md')) as f:
     readme = f.read()
 
 setup(name='analysis',
-      version='3.0',
+      version='4.0',
       description='Generic analysis package',
       long_description=readme,
       classifiers=[
@@ -29,18 +29,18 @@ setup(name='analysis',
           'Operating System :: MacOS',
           'Operating System :: Unix',
           'Programming Language :: Python :: 2.7',
-          # 'Programming Language :: Python :: 3.4',  # currently only py2 support
-          # 'Programming Language :: Python :: 3.5',
-          # 'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
           'Topic :: Scientific/Engineering :: Physics',
           ],
       keywords='HEP physics analysis toy fit',
       url='https://gitlab.cern.ch/apuignav/analysis-tools/',
-      author='Albert Puig, Jonas Eschle',  # example, + more...
-      author_email='albert.puig@cern.ch, jonas.eschle@cern.ch',  # example, + more...
+      author='Albert Puig, Jonas Eschle',
+      author_email='albert.puig@cern.ch, jonas.eschle@cern.ch',
       maintainer='Albert Puig',
       maintainer_email='albert.puig@cern.ch',
-      license='BSD3',
+      license='GPLv3',
       install_requires=['tables',
                         'pandas>=0.20.3',
                         'colorlog',
@@ -55,7 +55,7 @@ setup(name='analysis',
                         'matplotlib',
                         'seaborn'],
       packages=['analysis'],
-      data_files=['LICENSE'],
+      data_files=['LICENSE', 'README.md'],
       zip_safe=False)
 
-# EOFpi
+# EOF
