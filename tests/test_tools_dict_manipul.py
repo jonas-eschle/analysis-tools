01643549a98bbdb1d59a8a33b9866d5a5766ee99
Jonas Eschle 'Mayou36 <jonas.eschle@cern.ch>
2018-05-30 21:21:30 +0200

    Add unittest

--- tests/test_tools_dict_manipul.py
+++ tests/test_tools_dict_manipul.py
@@ -1,11 +1,13 @@
-from unittest import TestCase
 from collections import OrderedDict
 import copy
 
 import numpy as np
 import ROOT
+import pytest
+import yaml
+import yamlloader
 
-from analysis.utils.config import recursive_dict_copy
+from analysis.utils.config import recursive_dict_copy, replace_globals
 
 target1 = OrderedDict([('a', OrderedDict([('aa', {'aaa': 111, 'aab': OrderedDict(
         [('aaba', 1121), ('aabb', ROOT.RooRealVar('name1', 'title1', 3))])})])),
@@ -24,3 +26,6 @@ def test_recursive_dict_copy():
     assert test_odict1['b']['ba'] is target1['b']['ba']
     assert test_odict1['a']['aa']['aab']['aabb'] is target1['a']['aa']['aab']['aabb']
     assert isinstance(test_odict1['a']['aa']['aab'], OrderedDict)
+
+
+
